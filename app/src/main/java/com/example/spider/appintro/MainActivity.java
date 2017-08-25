f87d4bcd6590727e559f8b9670bd03b1f92c27f1
Spider34 <rakekum34@gmail.com>
2017-08-24 18:22:51 -0800

    Files submit

---
+++ app/src/main/java/com/example/spider/appintro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.spider.appintro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
